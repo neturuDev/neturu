3c2ac27b0bbc21896e0f5e7f7cf0269510737efd
Volodymyr Cherevchuk <cherevchuk.v@gmail.com>
2023-03-15 18:23:08 +0200

    Added icons

--- renderer/_default.page.server.jsx
+++ renderer/_default.page.server.jsx
@@ -2,8 +2,13 @@ import ReactDOMServer from 'react-dom/server'
 import React from 'react'
 import { PageShell } from './PageShell'
 import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr'
-import logoUrl from './logo.svg'
 import fontUrl from '../fonts/Comfortaa-VariableFont_wght.ttf'
+import faviconUrl from './icons/favicon.ico'
+import favicon16Url from './icons/favicon-16x16.png'
+import favicon32Url from './icons/favicon-32x32.png'
+import chromeIconUrl from './icons/android-chrome-192x192.png'
+import safariIconUrl from './icons/safari-pinned-tab.svg'
+import appleIconUrl from './icons/apple-touch-icon.png'
 
 export { render }
 // See https://vite-plugin-ssr.com/data-fetching
@@ -27,10 +32,20 @@ async function render(pageContext) {
     <html lang="${lang}">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" href="${logoUrl}" />
         <link rel="preload" href="${fontUrl}" as="font" type="font/ttf" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" />
         <meta name="description" content="${desc}" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="${favicon32Url}" />
+        <link rel="icon" type="image/png" sizes="192x192" href="${chromeIconUrl}" />
+        <link rel="icon" type="image/png" sizes="16x16" href="${favicon16Url}" />
+        <link rel="shortcut icon" href="${faviconUrl}" />
+        <link rel="mask-icon" href="${safariIconUrl}" color="#08259F" />
+        <link rel="apple-touch-icon" sizes="180x180" href="${appleIconUrl}" />
+        <meta name="application-name" content="Neturu" />
+        <meta name="apple-mobile-web-app-title" content="Neturu" />
         <title>${title}</title>
       </head>
       <body>
