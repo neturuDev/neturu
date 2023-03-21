26ea0c4367a2c8101a0eabcf93d8d692f3f7e9e9
Volodymyr Cherevchuk <cherevchuk.v@gmail.com>
2023-03-21 20:02:15 +0200

    Fixed styling issue

--- pages/articles/index.page.jsx
+++ pages/articles/index.page.jsx
@@ -14,23 +14,25 @@ function Page(pageProps) {
   const { posts } = pageProps;
   return (
     <>
-      <div className='blog-post-list'>
-        {posts.map((post) => {
-            const image = post.flatData.mainImage;
-            return(
-              <BlogItem
-                title={post.flatData.title}
-                slug={post.flatData.slug}
-                imageUrl={image[0].url}
-                imageId={image[0].id}
-                metaData={image[0].metadata}
-                previewText={post.flatData.preview}
-                publishedAt={post.flatData.publishedAt}
-                minutesToRead={post.flatData.minutesToReadStr}
-                key={post.id}
-              />
-            )
-          })}
+      <div className='content-container'>
+        <div className='blog-post-list'>
+          {posts.map((post) => {
+              const image = post.flatData.mainImage;
+              return(
+                <BlogItem
+                  title={post.flatData.title}
+                  slug={post.flatData.slug}
+                  imageUrl={image[0].url}
+                  imageId={image[0].id}
+                  metaData={image[0].metadata}
+                  previewText={post.flatData.preview}
+                  publishedAt={post.flatData.publishedAt}
+                  minutesToRead={post.flatData.minutesToReadStr}
+                  key={post.id}
+                />
+              )
+            })}
+        </div>
       </div>
     </>
   )
