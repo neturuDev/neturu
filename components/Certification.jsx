5122700b1aab4071efd2851a81e8eb5a9060a308
Volodymyr Cherevchuk <cherevchuk.v@gmail.com>
2023-08-27 10:55:38 +0300

    Finished certifications styling

--- components/Certification.jsx
+++ components/Certification.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import Date from './Date';
 
-export default function Certification({
-    title,
-    image,
-    issuingOrganization,
-    issued,
-    credentialID,
-    credentialLink,
-}) {
+export default function Certification({ title, image, issuingOrganization, issued, credentialLink }) {
     return (
-        <div className="certification">
-            <img
-                src={image.url}
-                alt={title}
-                width={image.metadata.pixelWidth}
-                height={image.metadata.pixelHeight}
-            />
-            <h3>{title}</h3>
-            <p>{issuingOrganization}</p>
-            <p>{credentialID}</p>
-            <Date dateString={issued} />
-            {credentialLink && (
-                <a href={credentialLink} target="_blank" rel="noreferrer">
-                    Show credential
-                </a>
-            )}
+        <div className='certification'>
+            <img src={image.url} alt={title} width={image.metadata.pixelWidth} height={image.metadata.pixelHeight} />
+            <div className='certification-data'>
+                <h3 className='certification-heading'>{title}</h3>
+                <div className='certification-subheading'>
+                    <span>{issuingOrganization}</span>
+                    <span>
+                        {'Issued'} <Date dateString={issued} />
+                    </span>
+                </div>
+                {credentialLink && (
+                    <div className='certification-link-wrapper'>
+                        <a className='certification-link' href={credentialLink} target='_blank' rel='noreferrer'>
+                            <div>{'Show credential'}</div>
+                            <div>
+                                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' data-supported-dps='16x16' fill='currentColor' width='16' height='16' focusable='false'>
+                                    <path d='M15 1v6h-2V4.41L7.41 10 6 8.59 11.59 3H9V1zm-4 10a1 1 0 01-1 1H5a1 1 0 01-1-1V6a1 1 0 011-1h2V3H5a3 3 0 00-3 3v5a3 3 0 003 3h5a3 3 0 003-3V9h-2z'></path>
+                                </svg>
+                            </div>
+                        </a>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
