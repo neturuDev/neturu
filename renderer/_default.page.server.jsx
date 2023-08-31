c93005bce3eac11939f848d21c9946a2fd6bd8d3
Volodymyr Cherevchuk <cherevchuk.v@gmail.com>
2023-08-31 18:15:31 +0300

    Fixed Preload font issue

--- renderer/_default.page.server.jsx
+++ renderer/_default.page.server.jsx
@@ -1,40 +1,40 @@
-import ReactDOMServer from 'react-dom/server'
-import React from 'react'
-import { PageShell } from './PageShell'
-import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr'
-import fontUrl from '../fonts/Comfortaa-VariableFont_wght.ttf'
-import faviconUrl from './icons/favicon.ico'
-import favicon16Url from './icons/favicon-16x16.png'
-import favicon32Url from './icons/favicon-32x32.png'
-import chromeIconUrl from './icons/android-chrome-192x192.png'
-import safariIconUrl from './icons/safari-pinned-tab.svg'
-import appleIconUrl from './icons/apple-touch-icon.png'
+import ReactDOMServer from 'react-dom/server';
+import React from 'react';
+import { PageShell } from './PageShell';
+import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr';
+import fontUrl from '../fonts/Comfortaa-VariableFont_wght.ttf';
+import faviconUrl from './icons/favicon.ico';
+import favicon16Url from './icons/favicon-16x16.png';
+import favicon32Url from './icons/favicon-32x32.png';
+import chromeIconUrl from './icons/android-chrome-192x192.png';
+import safariIconUrl from './icons/safari-pinned-tab.svg';
+import appleIconUrl from './icons/apple-touch-icon.png';
 
-export { render }
+export { render };
 // See https://vite-plugin-ssr.com/data-fetching
-export const passToClient = ['pageProps', 'urlPathname']
+export const passToClient = ['pageProps', 'urlPathname'];
 
 async function render(pageContext) {
-  const { Page, pageProps } = pageContext
-  const pageHtml = ReactDOMServer.renderToString(
-    <PageShell pageContext={pageContext}>
-      <Page {...pageProps} />
-    </PageShell>
-  )
+    const { Page, pageProps } = pageContext;
+    const pageHtml = ReactDOMServer.renderToString(
+        <PageShell pageContext={pageContext}>
+            <Page {...pageProps} />
+        </PageShell>
+    );
 
-  // See https://vite-plugin-ssr.com/head
+    // See https://vite-plugin-ssr.com/head
 
-   // Our custom export is available as `pageContext.exports.getDocumentProps`
-   const { getDocumentProps } = pageContext.exports
+    // Our custom export is available as `pageContext.exports.getDocumentProps`
+    const { getDocumentProps } = pageContext.exports;
 
-   // `getDocumentProps()` can use the fetched data to provide the page's meta data
-   const { title, description, lang } = getDocumentProps(pageProps)
+    // `getDocumentProps()` can use the fetched data to provide the page's meta data
+    const { title, description, lang } = getDocumentProps(pageProps);
 
-  const documentHtml = escapeInject`<!DOCTYPE html>
+    const documentHtml = escapeInject`<!DOCTYPE html>
     <html lang="${lang}">
       <head>
         <meta charset="UTF-8" />
-        <link rel="preload" href="${fontUrl}" as="font" type="font/ttf" />
+        <link rel="preload" href="${fontUrl}" as="font" type="font/ttf" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="index, follow" />
         <meta name="description" content="${description}" />
@@ -53,12 +53,12 @@ async function render(pageContext) {
       <body>
         <div id="page-view">${dangerouslySkipEscape(pageHtml)}</div>
       </body>
-    </html>`
+    </html>`;
 
-  return {
-    documentHtml,
-    pageContext: {
-      // We can add some `pageContext` here, which is useful if we want to do page redirection https://vite-plugin-ssr.com/page-redirection
-    }
-  }
+    return {
+        documentHtml,
+        pageContext: {
+            // We can add some `pageContext` here, which is useful if we want to do page redirection https://vite-plugin-ssr.com/page-redirection
+        },
+    };
 }
