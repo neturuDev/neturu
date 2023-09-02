3ecfb825f121a31973d7aff00a2a067c5fc0967c
Volodymyr Cherevchuk <cherevchuk.v@gmail.com>
2023-09-02 21:04:00 +0300

    Updated

--- renderer/_error.page.jsx
+++ renderer/_error.page.jsx
@@ -1,39 +1,29 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 import Error from '../components/Error';
 
-export { Page, getDocumentProps }
+export { Page };
 
-function setErrorData(is404) {
-  let errorHeading, errorDescription;
-  if (is404) {
-    errorHeading = '404 Page Not Found';
-    errorDescription = 'This page could not be found.';
-  } else {
-    errorHeading = '500 Internal Server Error';
-    errorDescription = 'Something went wrong.';
-  }
-
-  return {errorHeading, errorDescription};
-}
-
-function getDocumentProps({ is404 }) {
-  const {errorHeading, errorDescription} = setErrorData(is404);
-  return {
-    title: errorHeading,
-    description: errorDescription,
-    lang: 'en'
-  }
-}
-
+Page.propTypes = {
+    is404: PropTypes.bool,
+};
 function Page({ is404 }) {
-  const {errorHeading, errorDescription} = setErrorData(is404);
-
-  return (
-    <div className='content-container flex-center'>
-      <Error
-       heading={errorHeading}
-        description={errorDescription}
-      />
-    </div>
-  )
+    if (is404) {
+        return (
+            <div className='content-container flex-center'>
+                <Error
+                    heading={'404 Page Not Found'}
+                    description={'This page could not be found.'}
+                />
+            </div>
+        );
+    } else {
+        return (
+            <div className='content-container flex-center'>
+                <Error
+                    heading={'500 Internal Error'}
+                    description={'Something went wrong.'}
+                />
+            </div>
+        );
+    }
 }
--- renderer/usePageContext.jsx
+++ renderer/usePageContext.jsx
@@ -1,18 +1,25 @@
 // `usePageContext` allows us to access `pageContext` in any React component.
 // See https://vite-plugin-ssr.com/pageContext-anywhere
 
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { childrenPropType } from './PropTypeValues';
 
-export { PageContextProvider }
-export { usePageContext }
+export { PageContextProvider };
+// eslint-disable-next-line react-refresh/only-export-components
+export { usePageContext };
 
-const Context = React.createContext(undefined)
+const Context = React.createContext(undefined);
 
+PageContextProvider.propTypes = {
+    pageContext: PropTypes.any,
+    children: childrenPropType,
+};
 function PageContextProvider({ pageContext, children }) {
-  return <Context.Provider value={pageContext}>{children}</Context.Provider>
+    return <Context.Provider value={pageContext}>{children}</Context.Provider>;
 }
 
 function usePageContext() {
-  const pageContext = useContext(Context)
-  return pageContext
+    const pageContext = useContext(Context);
+    return pageContext;
 }
